23e97f612720509e2732acd68a5c105d9e950ec3
nailt <nailtarcan@gmail.com>
2020-01-30 13:11:58 -0500

    Got rid of deafult header in react navigation

--- App.js
+++ App.js
@@ -5,9 +5,23 @@ import SignupScreen from './screens/SignupScreen';
 
 /** Creating the navigator object which is needed to navigate between screens. */
 const MainNavigator = createStackNavigator({
-  Home : {screen : HomeScreen},
-  Signup: {screen : SignupScreen}
-})
+    Home : 
+    {
+      screen : HomeScreen,
+      navigationOptions:
+      {
+        headerShown : false
+      }
+    },
+    Signup: 
+    {
+      screen : SignupScreen,
+      navigationOptions:
+      {
+        headerShown : false
+      }
+    }
+});
 
 const App = createAppContainer(MainNavigator);
 export default App;
