ad5e5c75921aca847b3dfd5270b38c815012f943
nailt <nailtarcan@gmail.com>
2020-01-31 13:52:37 -0500

    Backend template added with design text.

---
+++ GigfinerBackend/src/main/java/gigfiner/GigfinerBackend/GigfinerBackendApplication.java
@@ -0,0 +1,13 @@
+package gigfiner.GigfinerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigfinerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigfinerBackendApplication.class, args);
+	}
+
+}
---
+++ GigfinerBackend/src/test/java/gigfiner/GigfinerBackend/GigfinerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gigfiner.GigfinerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigfinerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
